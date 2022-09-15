26e89781ed95ee1ab08734a058163280561c37c2
Anisa <An1sa_jon@mail.ru>
2022-09-15 15:47:05 +0400

    Task64

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int Recur(int n)
+{
+    return n < 10 ? 1 : 1 + Recur(n / 10);
+}
+void Print(string[] args)// метод вывода на печать
+{
+    Console.Write("Введите n=");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Recur(n));
+    Console.ReadKey(true);
+}
+
+Print(args);
