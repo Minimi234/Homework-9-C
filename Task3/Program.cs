b4b51b94964a2bc0e5d14213229268498f0cfc1d
Anisa <An1sa_jon@mail.ru>
2022-09-15 15:55:12 +0400

    Task68

---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿int Promt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int AckermanFunction(int M, int N)
+{
+    if (M == 0) return N + 1;
+    else if (M > 0 && N == 0) return AckermanFunction(M - 1, 1);
+    else if (M > 0 && N > 0) return AckermanFunction(M - 1, AckermanFunction(M, N - 1));
+    return 0;
+}
+
+int M = Promt("Введите неотрицательное число M > ");
+int N = Promt("Введите неотрицательное число N > ");
+
+if (M < 0 || N < 0)
+{
+    Console.WriteLine("Введите неотрицательные числа!!!");
+}
+else
+{
+    Console.WriteLine($"Значение функции Аккермана - А({M}, {N}) равно: {AckermanFunction(M, N)}");
+}
