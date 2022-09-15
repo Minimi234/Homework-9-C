8c923b5cb0a97bb5a3e4bc9ec0b8d6a6754bd57e
Anisa <An1sa_jon@mail.ru>
2022-09-15 15:50:24 +0400

    Task66

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int Promt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintEvenTwoNaturalList(int M, int N)
+{
+    if (N < M)
+    {
+        return;
+    }
+    PrintEvenTwoNaturalList(M, N - 1);
+    if (N % 2 == 0) Console.Write(N + " ");
+}
+
+int M = Promt("Введите натуральное число M > ");
+int N = Promt("Введите натуральное число N > ");
+PrintEvenTwoNaturalList(M, N);
